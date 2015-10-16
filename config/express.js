5bac6572581c70f431c3980eee2e6c300e010dc8
traciklopez <traciklopez@gmail.com>
2015-10-15 17:59:20 -0700

    user name and password routes/set up added

--- config/express.js
+++ config/express.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var config = require('./config'),
+	bodyParser = require('body-parser'),
+	express = require('express');
+
 module.exports = function() {
     var app = express();
+    
+    app.use(bodyParser.urlencoded({
+    	extended: true
+    }));
+
+    app.use(bodyParser.json());
+
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
+
     require('../app/routes/index.server.routes.js')(app);
+    require('../app/routes/users.server.routes.js')(app);
+
     app.use(express.static('./public'));
+    
     return app;
 };
